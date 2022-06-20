467219e86003154f03f562842aca1b44dbe86e07
hyemigwak <goadl92@gmail.com>
2022-06-21 02:26:00 +0900

    login추가

--- mynode/server.js
+++ mynode/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const multer  = require('multer');
+const bcrypt = require('bcrypt');
 
 const server = require('http').createServer(app);
 
@@ -25,9 +26,53 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(bodyParser.json());
 
+app.post("/login", (req, res) => {
+    const sql = "SELECT * FROM users WHERE email = $1";
+    pool.query(sql, [req.body.email], async (err, result) => {
+        if(err) {
+            return console.error(err.message);
+        }
+        if(result.rows.length === 0) {
+            res.json({ success: false, errorMessages: "id와 password가 일치하지 않습니다" });
+        } else {
+            const checkPassword = await bcrypt.compare(req.body.password, result.rows[0].password);
+            if(checkPassword) {
+                res.json({ success: true, email: req.body.email });
+            } else {
+                res.json({ success: false, errorMessages: "password가 일치하지 않습니다" })
+            }
+
+        }
+    })
+})
+
+app.post("/signup", (req, res) => {
+    const idCheckSql = "SELECT * FROM users WHERE email = $1"
+
+    pool.query(idCheckSql, [req.body.email], async (err,result) => {
+        if(err) {
+            return console.error(err.message);
+        }
+        if(result.rows.length !== 0) {
+            res.json({success: false, errorMessages: "이미 존재하는 email 입니다."});
+        } else {
+            //password를 해싱하고 저장한다.
+            const hashed = await bcrypt.hash(req.body.password, 10);
+            const hashedUserInfo = [req.body.email, hashed];
+            const sql = "INSERT INTO users (email, password) VALUES ($1, $2)"
+            pool.query(sql, hashedUserInfo, (err, result) => {
+                if(err) {
+                    return console.error(err.message);
+                }
+                res.json({success: true});
+            })
+        }
+    });
+
+});
+
 app.post("/image", upload.single("file"), (req, res) => {
     let imageUrl = `http://localhost:5000/uploads/${req.file.filename}`;
-    console.log(imageUrl,"imageUrl");
     res.json({success: true, data: imageUrl});
 })
 
@@ -98,11 +143,22 @@ const sql_create = `CREATE TABLE IF NOT EXISTS memos (
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );`;
 
+const sql_create_user = `CREATE TABLE IF NOT EXISTS users (
+    user_ID SERIAL PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(60) NOT NULL
+);`;
+
 
 pool.query(sql_create, [], (err, result) => {
     if(err) {
         return console.log(err.message);
     }
+    pool.query(sql_create_user, [], (err,result) => {
+        if(err) {
+            return console.log(err.message);
+        }
+    })
     console.log("successful connection");
     server.listen(5000, ()=>{
         console.log('server is running on 5000')
